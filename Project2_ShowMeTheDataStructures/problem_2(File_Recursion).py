dea222dcab3600c5e3d1f9e6dcae1dd3efb4a0b8
kaustuv-hub <kaustuv_majumder@hotmail.com>
2022-01-17 01:40:18 -0500

    Edge case added

--- Project2_ShowMeTheDataStructures/problem_2(File_Recursion).py
+++ Project2_ShowMeTheDataStructures/problem_2(File_Recursion).py
@@ -1,32 +1,41 @@
 def find_files(suffix, path):
     import os
     
-    if suffix == '' or len(os.listdir(path)) == 0:
-        return []
+    if os.path.isdir(path):
+        if suffix == '':
+            return []
+        
+        elements = os.listdir(path)
+        files = [file for file in elements if (suffix) in file]
+        folders = [folder for folder in elements if '.' not in folder]
     
-    elements = os.listdir(path)
-    files = [file for file in elements if (suffix) in file]
-    folders = [folder for folder in elements if '.' not in folder]
+        for folder in folders:
+            files.extend(find_files(suffix, path + '/' + folder))
     
-    for folder in folders:
-        files.extend(find_files(suffix, path + '/' + folder))
-    
-    return files  
+        return files
+    else:
+        return 'Unable to find specified path'
 
 
 # Testing preparation
 path_base = os.getcwd() + '/testdir'
 
-# Normal Cases:
+# Test Case 1:
 print(find_files(suffix='c', path=path_base))
 # [a.c', 'b.c', 'a.c', 't1.c']
 
+#Test Case 2:
 print(find_files(suffix='h', path=path_base))
 # ['a.h', 'b.h', 'a.h', 't1.h']
 
+#Test Case 3:
 print(find_files(suffix='z', path=path_base))
 # []
 
-# Edge Cases:
+# Test Case 4 (Edge case):
 print(find_files(suffix='', path=path_base))
 # []
+
+# Test Case 5 (Edge case):
+print(find_files(suffix='', path='gghd'))
+# Unable to find specified path
